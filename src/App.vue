476135fc6c3f01bb507e80f639d182d4d20f2bf2
CelsonF <celson338540@gmail.com>
2021-12-08 15:07:57 -0300

    Creating component nav and use efects transitions

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="corpo">
-   <nav>
-     <ul>
-       <li v-for="route in routes">
-          <router-link :to="route.path ? route.path : '/'">
-           {{ route.titulo}}
-          </router-link>
-       </li>
-     </ul>
-   </nav>
-    <router-view></router-view>
+    <meu-menu :rotas="routes"></meu-menu>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue'
 
 export default {
+  components: {
+     'meu-menu':Menu
+  },
+
   data() {
     return {
-      routes
+     routes
     }
   }
 
@@ -31,5 +30,14 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
+}
+
+
+.pagina-enter, .pagina-leave-active{
+ opacity: 0;
+}
+
+.pagina-enter-active , .pagina-leave-active {
+ transition: opacity 0.3s;
 }
 </style>
