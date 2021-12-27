d3c992f28ca3a3b4a6f9136ca62f33e1281563c8
CelsonF <celson338540@gmail.com>
2021-12-26 22:37:18 -0300

    create build project and translate msg vee-validator

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
-import Cadastro from './components/cadastro/Cadastro.vue'
 import Home from './components/home/Home.vue'
+
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
 
 export const routes = [
    {
